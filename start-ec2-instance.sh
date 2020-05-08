003197752890e505c44ac573ec64bae6224fa48a
LouisJackman <11330428+LouisJackman@users.noreply.github.com>
2020-05-08 10:49:45 +0100

    Add EC2 instance starter script

---
+++ start-ec2-instance.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# Start the first EC2 instance found with the name provided as the first
+# argument. If the region is omitted as the second argument, it is taken
+# from the default configuration.
+#
+
+set -o errexit
+set -o nounset
+
+readonly name="$1"
+shift
+
+readonly region="$2"
+shift
+
+start_ec2_instance() {
+    local name="$1"
+    shift
+
+    local region="$1"
+    shift
+
+    local region_arg
+    if [ -n "$region" ]
+    then
+        region_arg="--region '$region'"
+    fi
+
+    local instance_id="$(aws $region_arg --output json ec2 describe-instances \
+        --filters Name=tag:Name,Values="$name" \
+        --query "Reservations[*].Instances[*].InstanceId" \
+        | jq -r ".[0][0]"
+    )"
+    aws $region_arg ec2 start-instances --instance-ids "$instance_id"
+}
+
+start_ec2_instance "$name" "$region"
+
